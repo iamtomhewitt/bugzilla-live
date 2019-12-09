6feac27c57fb573a37d0569579f094d9661dcdcb
Tom Hewitt <tommhewitt@gmail.com>
2019-12-09 20:03:19 +0000

    💻 Separate out the routes

--- backend/app.js
+++ backend/app.js
@@ -2,38 +2,16 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
 
+var bugRoutes = require('./routes/bug-routes')
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Home
 app.get('/', function (req, res) {
     res.send('Bugzilla backend is running! :-)');
 });
 
-// BUGS
-// Get bugs by numbers
-app.get('/bugs/numbers/:numbers', function (req, res) {
-    var bugNumbers = req.params.numbers;
-    res.send('Requested bugs with numbers: ' + bugNumbers);
-    console.log('Bug numbers: ' + bugNumbers);
-});
-
-// Get bugs by username
-app.get('/bugs/username/:username', function (req, res) {
-    var username = req.params.username;
-    res.send('Requested bugs with username: ' + username);
-});
-
-// Get comments for a bug
-app.get('/bug/:number/comments', function (req, res) {
-    res.send('Requested comments for bug: ' + req.params.number);
-});
-
-// Change status for a bug
-app.post('/bug/:number/state/:state', function (req, res) {
-    res.send('Requested to change ' + req.params.number + ' state to: ' + req.params.state);
-});
-
+app.use('/bugs', bugRoutes);
 
 var port = 3000;
 app.listen(port, function () { });
