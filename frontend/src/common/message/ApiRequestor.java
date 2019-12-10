dce86af260c63cbedfcb84e6d9eb17b18b9cf032
Tom Hewitt <tommhewitt@gmail.com>
2019-12-10 15:49:19 +0000

    💻 Return response as a JSONObject

--- frontend/src/common/message/ApiRequestor.java
+++ frontend/src/common/message/ApiRequestor.java
@@ -6,6 +6,9 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  * An abstract class making requests to the Node Express backend.
@@ -14,17 +17,19 @@ import org.apache.http.util.EntityUtils;
  */
 public class ApiRequestor {
 	
-	public static String request(String url) {
+	public static JSONObject request(String url) {
 		try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
 			HttpGet request = new HttpGet(url);
 
 			HttpResponse result = httpClient.execute(request);
 
-			String json = EntityUtils.toString(result.getEntity(), "UTF-8");
+			String jsonString = EntityUtils.toString(result.getEntity(), "UTF-8");
+			
+			JSONObject json = (JSONObject) new JSONParser().parse(jsonString);
 
 			return json;
 
-		} catch (IOException ex) {
+		} catch (IOException | ParseException ex) {
 			ex.printStackTrace();
 		}
 		return null;
