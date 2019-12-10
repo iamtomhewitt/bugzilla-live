146817b7fe50751b0f70ba3bc1ce304f83902522
Tom Hewitt <tommhewitt@gmail.com>
2019-12-10 11:27:10 +0000

    💻 Build routing for saving config

--- backend/routes/config-routes.js
+++ backend/routes/config-routes.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var fs = require('fs');
 var path = require('path')
 
-var configFile = path.join(__dirname, '..', 'config', 'config.json');
+var configFilename = path.join(__dirname, '..', 'config', 'config.json');
+var configFile = require(configFilename)
 
 router.get('/', function (req, res) {
     res.status(200).send('OK');
@@ -11,13 +12,72 @@ router.get('/', function (req, res) {
 
 // Get existing config
 router.get('/get', function (req, res) {
-	let contents = fs.readFileSync(configFile, 'utf-8');
-    res.send(contents);
+	let contents = fs.readFileSync(configFilename, 'utf-8');
+	let response = success("Config retrieved", contents)
+    res.send(response);
 });
 
 // Save config
 router.get('/save', function (req, res) {
-    res.send('/save config');
+	let key 	= req.query.key;
+	let value 	= req.query.value;
+	let error;
+	let response;
+
+	if (!key || !value)	{
+		error = {
+			"title": "Missing parameters",
+			"message": "A key or value was missing."
+		}
+		response = failure(error);
+		res.send(response);
+		return;
+	}
+
+	configFile[key] = value;
+
+	fs.writeFile(configFilename, JSON.stringify(configFile, null, 4), function(err){
+		if (err) {
+			error = {
+				"title": "Error saving config",
+				"message": err.message
+			}
+			response = failure(error);
+			res.send(response);
+			return;
+		}
+	});
+
+	response = success("Config saved");
+    res.send(response);
 });
+
+function success(message) {
+	return response = {
+		"type": "configResponse",
+		"operation": "notification",
+		"message": message
+	}
+}
+
+function success(message, config) {
+	return response = {
+		"type": "configResponse",
+		"operation": "notification",
+		"message": message,
+		"config": config
+	}
+}
+
+function failure(error) {
+	return response = {
+		"type": "configResponse",
+		"operation": "notification",
+		"error": {
+			"title": error.title,
+			"message": error.message
+		}
+	}
+}
 
 module.exports = router;
