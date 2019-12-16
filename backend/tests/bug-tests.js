da9ef98d2df232618aa53404dacb3f47c9d44a9f
Tom Hewitt <tommhewitt@gmail.com>
2019-12-16 21:52:27 +0000

    🧪 Add some bug route tests

---
+++ backend/tests/bug-tests.js
@@ -0,0 +1,95 @@
+var request = require('supertest');
+var assert = require('assert');
+
+describe('Bug route tests', function () {
+	var server;
+	var timeout = 30000;
+
+	before(function () {
+		server = require('../app').listen(3002);
+	});
+
+	after(function () {
+		server.close();
+	});
+
+	it('Should give 200 when querying for a bug', function test(done) {
+		request(server)
+			.get('/bugs/numbers?numbers=12345')
+			.expect(200, done);
+	});
+
+	it('Should give 601 when no parameters are passed for bug numbers', function test(done) {
+		request(server)
+			.get('/bugs/numbers')
+			.expect(601, done);
+	});
+
+	it('Should have correct error title and message when no parameters are passed for bug numbers', function test(done) {
+		request(server)
+			.get('/bugs/numbers')
+			.expect(601)
+			.end(function(err, response) {
+				if (err) {
+					return done(err);
+				}
+
+				let error = response.body["error"];
+				assert.equal(error.title, 'Invalid bugs');
+				assert.equal(error.message, 'No numbers specified in query.');
+				done();
+			});
+	});
+
+	it('Should give 200 when getting bugs by email', function test(done) {
+		request(server)
+			.get('/bugs/email?email=test@gmail.com')
+			.expect(200, done);
+	}).timeout(timeout);
+
+	it('Should give 601 when no parameters are passed for bugs with email', function test(done) {
+		request(server)
+			.get('/bugs/email')
+			.expect(601, done);
+	});
+
+	it('Should have correct error title and message when no parameters are passed for email', function test(done) {
+		request(server)
+			.get('/bugs/email')
+			.expect(601)
+			.end(function(err, response) {
+				if (err) {
+					return done(err);
+				}
+
+				let error = response.body["error"];
+				assert.equal(error.title, 'Invalid email');
+				assert.equal(error.message, 'No email specified in query.');
+				done();
+			});
+	});
+
+	it('Should give 200 when getting comments for a bug', function test(done) {
+		request(server)
+			.get('/bugs/12345/comments')
+			.expect(200, done);
+	}).timeout(timeout);
+
+	it('Should give 404 when getting comments with no number parameter', function test(done) {
+		request(server)
+			.get('/bugs//comments')
+			.expect(404, done);
+	});
+
+	it('Should give 200 when getting attachments for a bug', function test(done) {
+		request(server)
+			.get('/bugs/12345/attachments')
+			.expect(200, done);
+	}).timeout(timeout);
+
+	it('Should give 404 when getting attachments with no number parameter', function test(done) {
+		request(server)
+			.get('/bugs//attachments')
+			.expect(404, done);
+	});
+});
