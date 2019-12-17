c3aa5a268a275cd4377e69c5ec1e2d27ea320136
Tom Hewitt <tommhewitt@gmail.com>
2019-12-17 21:28:23 +0000

    🧪 Remove unused method, and add example bug list to satisfy testing

--- backend/tests/list-tests.js
+++ backend/tests/list-tests.js
@@ -13,17 +13,6 @@ describe('List route tests', function () {
 	});
 
 	after(function () {
-		// Remove test files
-		fs.readdir(directory, (err, files) => {
-			if (err) throw err;
-		  
-			for (const file of files) {
-				fs.unlink(path.join(directory, file), err => {
-					if (err) throw err;
-				});
-			}
-		});
-
 		server.close();
 	});
 
