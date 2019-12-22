32868071ee7349b85f6b5d7b81726e87b6c8ede9
Tom Hewitt <tommhewitt@gmail.com>
2019-12-22 19:31:05 +0000

    💻 Add zip command to build

--- build/build.sh
+++ build/build.sh
@@ -5,3 +5,7 @@ nexe app.js -n "../dist/bugzilla-live-backend"
 # Build the frontend
 cd ../frontend
 ant build_frontend
+
+# Now zip everything up for the release
+cd ..
+zip -r dist/bugzilla-live.zip dist
