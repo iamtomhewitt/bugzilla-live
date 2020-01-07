42c1247b5ac829785f5d3df48c24ad2021f1694a
Tom Hewitt <tommhewitt@gmail.com>
2020-01-07 14:25:00 +0000

    💎 Lint the backend

--- backend/routes/config-routes.js
+++ backend/routes/config-routes.js
@@ -1,86 +1,35 @@
-var express = require('express')
-var fs = require('fs');
-var path = require('path')
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
-var router = express.Router();
-var configFilename = path.join(__dirname, '..', 'config', 'config.json');
-var configFile = require(configFilename)
-var errorCode = 601;
-var successCode = 200;
+const router = express.Router();
+const configFilename = path.join(__dirname, '..', 'config', 'config.json');
+const configFile = require(configFilename);
+const errorCode = 601;
+const successCode = 200;
 
-router.get('/', function (req, res) {
-    res.status(200).send('OK');
-});
-
-// Get existing config
-router.get('/get', function (req, res) {
-	let contents = fs.readFileSync(configFilename, 'utf-8');
-	let response = success(contents)
-    res.status(successCode).send(response);
-});
-
-// Save config
-router.get('/save', function (req, res) {
-	let key 	= req.query.key;
-	let value 	= req.query.value;
-	let error;
-	let response;
-
-	if (!key || !value)	{
-		error = createError("Missing parameters", "A key or value was missing.")
-		response = failure(error);
-		res.status(errorCode).send(response);
-		return;
-	}
-
-	value = IsJsonString(value) ? JSON.parse(value) : value;
-	configFile[key] = value;
-
-	fs.writeFile(configFilename, JSON.stringify(configFile, null, 4), function(err){
-		if (err) {
-			error = createError("Error saving config", err.message);
-			response = failure(error);
-			res.status(errorCode).send(response);
-			return;
-		}
-	});
-
-	response = success("Config saved");
-    res.status(successCode).send(response);
-});
-
-function success(message) {
-	return response = {
-		"type": "configResponse",
-		"operation": "notification",
-		"status": successCode,
-		"message": message
-	}
-}
-
-function success(config) {
-	return response = {
-		"type": "configResponse",
-		"operation": "notification",
-		"status": successCode,
-		"config": config
-	}
+function success() {
+    return {
+        type: 'configResponse',
+        operation: 'notification',
+        status: successCode,
+    };
 }
 
 function failure(error) {
-	return response = {
-		"type": "configResponse",
-		"operation": "notification",
-		"status": errorCode,
-		"error": error
-	}
+    return {
+        type: 'configResponse',
+        operation: 'notification',
+        status: errorCode,
+        error,
+    };
 }
 
 function createError(title, message) {
-	return error = {
-		"title": title,
-		"message": message
-	}
+    return {
+        title,
+        message,
+    };
 }
 
 function IsJsonString(str) {
@@ -91,5 +40,48 @@ function IsJsonString(str) {
     }
     return true;
 }
+
+router.get('/', (req, res) => {
+    res.status(200).send('OK');
+});
+
+// Get existing config
+router.get('/get', (req, res) => {
+    const contents = fs.readFileSync(configFilename, 'utf-8');
+    const response = success(contents);
+    response.config = contents;
+    res.status(successCode).send(response);
+});
+
+// Save config
+router.get('/save', (req, res) => {
+    const key = req.query.key;
+    let value = req.query.value;
+    let error;
+    let response;
+
+    if (!key || !value) {
+        error = createError('Missing parameters', 'A key or value was missing.');
+        response = failure(error);
+        res.status(errorCode).send(response);
+        return;
+    }
+
+    value = IsJsonString(value) ? JSON.parse(value) : value;
+    configFile[key] = value;
+
+    fs.writeFile(configFilename, JSON.stringify(configFile, null, 4), (err) => {
+        if (err) {
+            error = createError('Error saving config', err.message);
+            response = failure(error);
+            res.status(errorCode).send(response);
+        }
+    });
+
+    response = success();
+    response.message = 'Config saved';
+    res.status(successCode).send(response);
+});
+
 
 module.exports = router;
