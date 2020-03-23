4f6acb3eae8f18c703895fff88fb62dea57f2007
Tom Hewitt <tommhewitt@gmail.com>
2020-03-23 15:24:46 +0000

    🥾 Add sprint boot project

---
+++ backend/src/main/java/bugzillalive/BugzillaLiveBackend.java
@@ -0,0 +1,11 @@
+package bugzillalive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugzillaLiveBackend {
+	public static void main(String[] args) {
+		SpringApplication.run(BugzillaLiveBackend.class, args);
+	}
+}
