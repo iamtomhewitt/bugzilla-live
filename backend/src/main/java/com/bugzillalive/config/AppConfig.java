4f67aea6f0cb465c641160763ad5c1c36cfe13a8
Tom Hewitt <tommhewitt@gmail.com>
2020-04-03 08:37:34 +0100

    💎 refactor(backend): Rename to AppConfig

--- backend/src/main/java/com/bugzillalive/config/AppConfig.java
+++ backend/src/main/java/com/bugzillalive/config/AppConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 import java.time.Duration;
 
 @Configuration
-public class Config {
+public class AppConfig {
 
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
