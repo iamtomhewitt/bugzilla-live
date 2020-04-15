fd0718ef5136a783ad2dea9c6929a653bd443f03
Tom Hewitt <tommhewitt@gmail.com>
2020-04-15 14:33:09 +0100

    💻 chore(frontend): Add fetching of lists to api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -11,3 +11,13 @@ export async function getConfig() {
 	return fetch(config.backendUrl + '/config/get')
 		.then(response => response.json())
 }
+
+export async function getLists() {
+	return fetch(config.backendUrl + '/lists/all')
+		.then(response => response.json())
+}
+
+export async function getCurrentList() {
+	return fetch(config.backendUrl + '/lists/current')
+		.then(response => response.json())
+}
