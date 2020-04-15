6a9942cc0a624e5dbaa5e74914ca59eb1c53fb98
Tom Hewitt <tommhewitt@gmail.com>
2020-04-15 14:34:06 +0100

    💻 chore(routes): Add the lists page to routes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Route, Switch } from 'react-router-dom';
 import Home from "./components/home/Home";
 import PageNotFound from "./components/page-not-found/PageNotFound";
+import Lists from "./components/lists/Lists";
 
 export default () =>
 	<Switch>
 		<Route path="/" exact component={Home} />
+		<Route path="/lists" exact component={Lists} />
 
 		{ /* Catch all unmatched routes */}
 		<Route component={PageNotFound} />
