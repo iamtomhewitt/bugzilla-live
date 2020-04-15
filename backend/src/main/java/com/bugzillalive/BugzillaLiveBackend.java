c64e1d14d5570d9b92cd05163e4b189c31d0941f
Tom Hewitt <tommhewitt@gmail.com>
2020-04-15 19:34:32 +0100

    📝 docs(backend): Add swagger to backend

--- backend/src/main/java/com/bugzillalive/BugzillaLiveBackend.java
+++ backend/src/main/java/com/bugzillalive/BugzillaLiveBackend.java
@@ -2,8 +2,10 @@ package com.bugzillalive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BugzillaLiveBackend {
 	public static void main(String[] args) {
 		SpringApplication.run(BugzillaLiveBackend.class, args);
