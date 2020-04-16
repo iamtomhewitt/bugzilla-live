d4a6e9900d6922d7e661f16225e1e53709d688a7
Tom Hewitt <tommhewitt@gmail.com>
2020-04-16 08:12:42 +0100

    💎 refactor(frontend): Remove unused App css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,10 @@ import React from 'react';
 import Routes from './Routes';
 import Navigation from './components/navbar/Navigation';
 
-import './App.css';
-
 export default class App extends React.Component {
-
 	render() {
 		return (
-			<div className='App'>
+			<div>
 				<Navigation/>
 				<Routes/>
 			</div>
