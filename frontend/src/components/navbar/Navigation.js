1779ca2d9816cef1a2391103d10a1aba7036c46c
Tom Hewitt <tommhewitt@gmail.com>
2020-04-17 11:16:02 +0100

    🎨 style(navbar): Update to react-bootstrap navbar

--- frontend/src/components/navbar/Navigation.js
+++ frontend/src/components/navbar/Navigation.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 import './Navigation.css'
 
 export default class Navigation extends Component {
-
 	render() {
 		return (
-			<ul>
-				<li><a href="/" id="home">Bugzilla Live</a></li>
-				<li><a href="/lists">Lists</a></li>
-				<li><a href="/config">Config</a></li>
-			</ul>
+			<Navbar bg="primary" variant="dark">
+				<Navbar.Brand href="/">Bugzilla LIVE</Navbar.Brand>
+				<Nav className="mr-auto">
+					<Nav.Link href="/lists">Lists</Nav.Link>
+					<Nav.Link href="/config">Config</Nav.Link>
+				</Nav>
+			</Navbar>
 		)
 	}
 }
