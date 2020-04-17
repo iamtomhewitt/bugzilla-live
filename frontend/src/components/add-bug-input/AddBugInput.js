6b8437244b6e876c6eccd984c266f95ec537cd7a
Tom Hewitt <tommhewitt@gmail.com>
2020-04-17 11:16:22 +0100

    🎨 style(add-bug): Update to react-bootstrap button

--- frontend/src/components/add-bug-input/AddBugInput.js
+++ frontend/src/components/add-bug-input/AddBugInput.js
@@ -1,34 +1,40 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
 import * as api from '../../api/api';
 import './AddBugInput.css'
 
 export default class AddBugInput extends React.Component {
-    constructor(props) {
-        super(props);
-        
-        this.updateBugNumbers = this.updateBugNumbers.bind(this)
-        this.handleChange = this.handleChange.bind(this)
-    }
+	constructor(props) {
+		super(props);
 
-    handleChange = event => {
-        this.setState({
-            input: event.target.value
-        });
-    }
+		this.updateBugNumbers = this.updateBugNumbers.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+	}
 
-    async updateBugNumbers() {
+	handleChange = event => {
+		this.setState({
+			input: event.target.value
+		});
+	}
+
+	async updateBugNumbers() {
 		const currentList = await api.getCurrentList();
 		const newContent = currentList.content + ',' + this.state.input;
-		await api.updateCurrentList({name: currentList.name, content: newContent})
-        this.props.updateBugs()
-    }
+		await api.updateCurrentList({ name: currentList.name, content: newContent })
+		this.props.updateBugs()
+	}
 
-    render() {
-        return (
-            <div id='addBugContainer'>
-                <input id="addBugInput" type="number" onChange={this.handleChange}/>
-                <button id="addBugButton" onClick={this.updateBugNumbers}>Add</button>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div id='addBugContainer'>
+				<input id="addBugInput" type="number" onChange={this.handleChange} />
+				<Button
+					id="addBugButton"
+					variant="primary"
+					onClick={(e) => this.updateBugNumbers}>
+					Add
+				</Button>
+			</div>
+		);
+	}
 }
