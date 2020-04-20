eabfeb32b8f5df91507e0c372c9c4fe6052d9c48
Tom Hewitt <tommhewitt@gmail.com>
2020-04-20 16:19:45 +0100

    🎨 style(attachment): Open attachment in new tab

--- frontend/src/components/modal/attachment/AttachmentModal.js
+++ frontend/src/components/modal/attachment/AttachmentModal.js
@@ -32,7 +32,7 @@ export default class AttachmentModal extends React.Component {
 					<Modal.Body>
 						{this.state.attachments.map((attachment) => {
 							return <div key={attachment['filename']}>
-								<a href={this.state.bugzillaUrl + "attachment.cgi?id=" + attachment['id']}>{attachment['filename']}</a>
+								<a target="_blank" rel="noopener noreferrer" href={this.state.bugzillaUrl + "attachment.cgi?id=" + attachment['id']}>{attachment['filename']}</a>
 							</div>
 						})}
 						{this.state.attachments.length === 0 &&
