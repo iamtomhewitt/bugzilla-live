f29e5051072306300c07291ddce5f08e29918d3f
Tom Hewitt <tommhewitt@gmail.com>
2020-04-20 16:05:51 +0100

    💻 feat(frontend): Add a hyperlink to open the attachment in a new tab

--- frontend/src/components/modal/attachment/AttachmentModal.js
+++ frontend/src/components/modal/attachment/AttachmentModal.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, Modal } from 'react-bootstrap';
 import * as api from '../../../api/api';
+
 import './AttachmentModal.css'
 
 export default class AttachmentModal extends React.Component {
@@ -14,7 +15,11 @@ export default class AttachmentModal extends React.Component {
 
 	async componentDidMount() {
 		const attachments = await api.getBugAttachments(this.props.bug['id']);
-		this.setState({ attachments })
+		const config = await api.getConfig();
+		this.setState({
+			attachments,
+			bugzillaUrl: config.bugzillaUrl
+		})
 	}
 
 	render() {
@@ -27,12 +32,11 @@ export default class AttachmentModal extends React.Component {
 					<Modal.Body>
 						{this.state.attachments.map((attachment) => {
 							return <div key={attachment['filename']}>
-								<p id="name">{attachment['filename']}</p>
-								<p>{attachment['contentType']}</p>
+								<p id="name">{attachment['filename']} <a href={this.state.bugzillaUrl + "attachment.cgi?id=" + attachment['id']}>Open</a></p>
 							</div>
 						})}
 						{this.state.attachments.length === 0 &&
-							<div id="name"><p>There are no attachments for this bug.</p></div>
+							<div id="noAttachments"><p>There are no attachments for this bug.</p></div>
 						}
 					</Modal.Body>
 					<Modal.Footer id="footer">
