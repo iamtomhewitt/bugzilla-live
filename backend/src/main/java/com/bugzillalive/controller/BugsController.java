ffa40fa03b97fbf53a55e94d102551732ed8aa55
Tom Hewitt <tommhewitt@gmail.com>
2020-09-19 21:57:59 +0100

    refactor(#68): update bug controller and service

--- backend/src/main/java/com/bugzillalive/controller/BugsController.java
+++ backend/src/main/java/com/bugzillalive/controller/BugsController.java
@@ -7,7 +7,6 @@ import com.bugzillalive.model.bug.Comment;
 import com.bugzillalive.model.request.AddCommentRequestBody;
 import com.bugzillalive.model.request.ChangeStatusRequestBody;
 import com.bugzillalive.service.BugsService;
-import com.bugzillalive.service.ConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,61 +16,48 @@ import java.util.List;
 
 @RestController
 @RequestMapping("bugs")
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "*")
 public class BugsController {
 	@Autowired
 	private BugsService service;
 
-	@Autowired
-	private ConfigService configService;
-
-	@GetMapping("/health")
+	@GetMapping("/{numbers}")
 	@ResponseBody
-	public ResponseEntity<String> health() {
-		return new ResponseEntity<>("UP", HttpStatus.OK);
-	}
-
-	@GetMapping("/numbers")
-	@ResponseBody
-	public ResponseEntity<List<Bug>> getBugsByNumbers(@RequestParam String numbers) throws ConfigNotFoundException {
-		String url = configService.getBugzillaUrl() + "/rest/bug?id=";
-		List<Bug> bugs = service.getBugsByNumbers(url, numbers);
+	public ResponseEntity<List<Bug>> getBugsByNumbers(@PathVariable String numbers) throws ConfigNotFoundException {
+		List<Bug> bugs = service.getBugsByNumbers(numbers);
 		return new ResponseEntity<>(bugs, HttpStatus.OK);
 	}
 
-	@GetMapping("/username")
+	@GetMapping("/user/{username}")
 	@ResponseBody
-	public ResponseEntity<List<Bug>> getBugsByUsername(@RequestParam String username) throws ConfigNotFoundException {
-		String url = configService.getBugzillaUrl() + "/rest/bug?assigned_to=";
-		List<Bug> bugs = service.getBugsByUsername(url, username);
+	public ResponseEntity<List<Bug>> getBugsByUsername(@PathVariable String username) throws ConfigNotFoundException {
+		List<Bug> bugs = service.getBugsByUsername(username);
 		return new ResponseEntity<>(bugs, HttpStatus.OK);
 	}
 
 	@GetMapping("/{number}/comments")
 	@ResponseBody
 	public ResponseEntity<List<Comment>> getCommentsForBug(@PathVariable String number) throws ConfigNotFoundException {
-		String url = configService.getBugzillaUrl() + "/rest/bug/" + number + "/comment";
-		List<Comment> comments = service.getCommentsForBug(url, number);
+		List<Comment> comments = service.getCommentsForBug(number);
 		return new ResponseEntity<>(comments, HttpStatus.OK);
 	}
 
 	@GetMapping("/{number}/attachments")
 	@ResponseBody
 	public ResponseEntity<List<Attachment>> getAttachmentsForBug(@PathVariable String number) throws ConfigNotFoundException {
-		String url = configService.getBugzillaUrl() + "/rest/bug/" + number + "/attachment";
-		List<Attachment> attachments = service.getAttachmentsForBug(url, number);
+		List<Attachment> attachments = service.getAttachmentsForBug(number);
 		return new ResponseEntity<>(attachments, HttpStatus.OK);
 	}
 
 	@PostMapping("/{number}/comments/add")
 	@ResponseBody
 	public ResponseEntity<String> addCommentToBug(@PathVariable String number, @RequestBody AddCommentRequestBody body) throws ConfigNotFoundException {
-		return service.addCommentToBug(configService.getBugzillaUrl() + "/rest/bug/" + number + "/comment", body);
+		return service.addCommentToBug(number, body);
 	}
 
 	@PutMapping("/{number}/status/change")
 	@ResponseBody
 	public ResponseEntity<String> changeBugStatus(@PathVariable String number, @RequestBody ChangeStatusRequestBody body) throws ConfigNotFoundException {
-		return service.changeBugStatus(configService.getBugzillaUrl() + "/rest/bug/" + number, body);
+		return service.changeBugStatus(number, body);
 	}
 }
